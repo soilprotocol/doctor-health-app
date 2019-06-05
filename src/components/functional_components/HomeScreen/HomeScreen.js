c4b10848eda789861510f0bd58eeb2ccf3f2813b
Ludwig Schubert <ludwigschubert@Ludwigs-Mac-Pro.local>
2019-06-05 18:37:11 +0200

    Add hello world component

---
+++ src/components/functional_components/HomeScreen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HomeScreen.module.css"
+
+const HomeScreen = () => {
+    return (
+        <div className={styles.text}>
+            <h1>Hello World!</h1>
+        </div>
+    )
+};
+
+export default HomeScreen
