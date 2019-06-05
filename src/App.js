82d27636e562f89f7c192c3b5fe2295ed3e315a1
b4n4n31987 <malte.sielski@icloud.com>
2019-06-05 19:56:13 +0200

    Login & Auth

--- src/App.js
+++ src/App.js
@@ -3,18 +3,64 @@ import HomeScreen from "./components/functional_components/HomeScreen";
 import RequestScreen from "./components/functional_components/RequestScreen"
 import { BrowserRouter, Route, Switch } from "react-router-dom"; 
 import Navigation from './components/functional_components/Navbar/Navbar'; 
+import auth from 'solid-auth-client'; 
 
 
-function App() {
-  return (
-    <BrowserRouter>
-        <Navigation/>
-        <Switch>
-          <Route exact path ="/" component={HomeScreen} /> 
-          <Route exact path ="/request" component={RequestScreen} /> 
-        </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      webId: undefined
+    }
+  }
+
+  async login() {
+    const session = await auth.currentSession(); 
+    if(!session) {
+      await auth.login("https://solid.community"); 
+    } else {
+      this.setState({
+        webId: session.webId
+      });
+    }
+  }
+
+  logout() {
+    auth.logout().then(() => {
+      this.setState({
+        webId: undefined
+      });
+    }); 
+  }
+
+  componentDidMount () {
+    auth.trackSession(session => {
+      if(!session) {
+        console.log("You are logged out"); 
+      } else {
+        console.log("You are logged in, fetching data now");
+        this.setState({
+          webId: session.webId
+        }); 
+        console.log(this.state.webId); 
+        }
+      });
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+          <Navigation onClick={
+              this.state.webId ? this.logout.bind(this) : this.login.bind(this)
+            }
+          webid={this.state.webId}/>
+          <Switch>
+            <Route exact path ="/" component={HomeScreen} /> 
+            <Route exact path ="/request" component={RequestScreen} /> 
+          </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/functional_components/Navbar/Navbar.js
+++ src/components/functional_components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { Link, Navlink } from 'react-router-dom'; 
-import { Button } from 'react-bootstrap/Button'; 
+import Button from 'react-bootstrap/Button'; 
 import Navbar from 'react-bootstrap/Navbar'; 
 import Nav from 'react-bootstrap/Nav';
 
@@ -10,12 +10,12 @@ import Nav from 'react-bootstrap/Nav';
 const Navigation = props => {
     return (
     <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Startseite</Navbar.Brand>
         <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
+        <Button variant="primary" onClick={props.onClick}>{ props.webId? "Logout" : "Login"}</Button>
     </Navbar>
     )
 }
